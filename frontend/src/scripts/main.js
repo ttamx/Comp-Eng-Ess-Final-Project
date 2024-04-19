28d731d8fd860c35a0a5d089a58b6cc6dc07f5e9
ttamx <teetat.tata@gmail.com>
2024-04-19 12:55:48 +0700

    chore: ?

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -10,6 +10,13 @@ const config = {
             debug: false
         }
     },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'phaser-example',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
     scene: [GameScene]
 };
 
